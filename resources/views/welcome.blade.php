b1e0b58f21af8457071eefd3be5d188670b04c76
Quratulain Memon <quratulainmemon18@gmail.com>
2023-07-11 16:36:13 +0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Laravel</title>
+</head>
+<body>
+    <h1>Welcome to Laravel</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello, World!';
+});
